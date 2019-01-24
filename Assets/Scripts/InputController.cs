ae2d7068bbd4e3195216b645cb2d1bf3bb06a5fe
Dendi <freshmindfreak@gmail.com>
2019-01-24 22:59:16 +0100

    Project Upload
    Signed-off-by: Dendi <freshmindfreak@gmail.com>

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+	private static InputController instance = null;
+	public static InputController Instance {
+		get {
+			if (instance == null)
+				instance = FindObjectOfType<InputController>();
+			return instance;
+		}
+	}
+
+	public Action<int> TapHappened = delegate { };
+	public Action<int> ReleaseHappened = delegate { };
+
+	private void Update() {
+		// PC
+		if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) {
+			TapHappened(0);
+		}
+		if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.Space)) {
+			ReleaseHappened(0);
+		}
+	}
+}
