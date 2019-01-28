ffcbaec11cf691f4b26c46ca6dea8bdf067f1571
Dendi <freshmindfreak@gmail.com>
2019-01-29 00:22:17 +0100

    Level Serialization v0.5

---
+++ Assets/Scripts/Editor/LevelWindow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class LevelWindow : EditorWindow {
+
+	[MenuItem("Rope City/Level Saver")]
+	public static void ShowWindow() {
+		GetWindow<LevelWindow>(false, "Level Window", true);
+	}
+
+	string levelName = "";
+
+	void OnGUI() {
+		levelName = EditorGUILayout.TextField("Level Name", levelName);
+		GUI.backgroundColor = Color.green;
+		GUILayout.FlexibleSpace();
+		if (GUILayout.Button("Save Level")) {
+			LevelSerializer.SerializeCurrentlyOpenedLevel(levelName);
+		}
+
+	}
+}
