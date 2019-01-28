bacf5c1f99a2710fb53c74d25cbe2618df972ed3
Dendi <freshmindfreak@gmail.com>
2019-01-29 00:22:17 +0100

    Level Serialization v0.5

--- Assets/Scripts/Level/LevelSerializer.cs
+++ Assets/Scripts/Level/LevelSerializer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using SimpleJSON;
 using System.IO;
 
-public static class LevelSerializer {
+public class LevelSerializer {
 
 	private const string levelNameKey = "lvlName";
 	private const string levelObjectsKey = "lvlObjects";
@@ -16,19 +16,44 @@ public static class LevelSerializer {
 	private const string rotYKey = "RY";
 	private const string rotZKey = "RZ";
 
+	public static void SerializeCurrentlyOpenedLevel(string levelName) {
+
+		LevelObject[] levelObjects = Object.FindObjectsOfType<LevelObject>();
+		LevelObjectData[] levelObjectDatas = new LevelObjectData[levelObjects.Length];
+		for (int i = 0; i < levelObjects.Length; i++) {
+
+			levelObjectDatas[i] = new LevelObjectData();
+
+			levelObjectDatas[i].uniqueID = levelObjects[i].objectID;
+			levelObjectDatas[i].posX = Mathf.FloorToInt(levelObjects[i].transform.position.x);
+			levelObjectDatas[i].posY = Mathf.FloorToInt(levelObjects[i].transform.position.y);
+			levelObjectDatas[i].posZ = Mathf.FloorToInt(levelObjects[i].transform.position.z);
+
+			levelObjectDatas[i].rotX = Mathf.FloorToInt(levelObjects[i].transform.rotation.eulerAngles.x);
+			levelObjectDatas[i].rotY = Mathf.FloorToInt(levelObjects[i].transform.rotation.eulerAngles.y);
+			levelObjectDatas[i].rotZ = Mathf.FloorToInt(levelObjects[i].transform.rotation.eulerAngles.z);
+		}
+
+		LevelData data = new LevelData();
+		data.levelName = levelName;
+		data.levelObjects = levelObjectDatas;
+
+		SerializeLevel(data);
+
+	}
 
 	public static void SerializeLevel(LevelData level) {
 		JSONObject levelJson = new JSONObject();
 		levelJson[levelNameKey] = level.levelName;
 
 		for (int i = 0; i < level.levelObjects.Length; i++) {
-			levelJson[levelObjectsKey][level.levelObjects[i].uniqueID][posXKey].AsInt = level.levelObjects[i].posX;
-			levelJson[levelObjectsKey][level.levelObjects[i].uniqueID][posYKey].AsInt = level.levelObjects[i].posY;
-			levelJson[levelObjectsKey][level.levelObjects[i].uniqueID][posZKey].AsInt = level.levelObjects[i].posZ;
+			levelJson[levelObjectsKey][i][level.levelObjects[i].uniqueID][posXKey].AsInt = level.levelObjects[i].posX;
+			levelJson[levelObjectsKey][i][level.levelObjects[i].uniqueID][posYKey].AsInt = level.levelObjects[i].posY;
+			levelJson[levelObjectsKey][i][level.levelObjects[i].uniqueID][posZKey].AsInt = level.levelObjects[i].posZ;
 
-			levelJson[levelObjectsKey][level.levelObjects[i].uniqueID][rotXKey].AsInt = level.levelObjects[i].rotX;
-			levelJson[levelObjectsKey][level.levelObjects[i].uniqueID][rotYKey].AsInt = level.levelObjects[i].rotY;
-			levelJson[levelObjectsKey][level.levelObjects[i].uniqueID][rotZKey].AsInt = level.levelObjects[i].rotZ;
+			levelJson[levelObjectsKey][i][level.levelObjects[i].uniqueID][rotXKey].AsInt = level.levelObjects[i].rotX;
+			levelJson[levelObjectsKey][i][level.levelObjects[i].uniqueID][rotYKey].AsInt = level.levelObjects[i].rotY;
+			levelJson[levelObjectsKey][i][level.levelObjects[i].uniqueID][rotZKey].AsInt = level.levelObjects[i].rotZ;
 		}
 
 		string jsonString = levelJson.ToString();
