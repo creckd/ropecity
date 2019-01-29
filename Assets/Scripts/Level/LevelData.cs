e90d7132b1df121b7d6e2fb7cd08389bb4ab67e0
Dendi <freshmindfreak@gmail.com>
2019-01-29 17:47:41 +0100

    Level Serialization fully working!

--- Assets/Scripts/Level/LevelData.cs
+++ Assets/Scripts/Level/LevelData.cs
@@ -5,6 +5,10 @@
 
 	public static bool operator ==(LevelData a, LevelData b) {
 		bool theyAreEqual = true;
+		if (ReferenceEquals(a,null) && ReferenceEquals(b,null))
+			return true;
+		if ((ReferenceEquals(a,null) && !ReferenceEquals(b,null)) || (ReferenceEquals(a,null) && !ReferenceEquals(b,null)))
+			return false;
 		if (a.levelName != b.levelName)
 			theyAreEqual = false;
 		if (a.levelObjects.Length != b.levelObjects.Length)
@@ -15,6 +19,10 @@
 
 	public static bool operator !=(LevelData a, LevelData b) {
 		bool theyAreEqual = true;
+		if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+			return false;
+		if ((ReferenceEquals(a, null) && !ReferenceEquals(b, null)) || (ReferenceEquals(a, null) && !ReferenceEquals(b, null)))
+			return true;
 		if (a.levelName != b.levelName)
 			theyAreEqual = false;
 		if (a.levelObjects.Length != b.levelObjects.Length)
