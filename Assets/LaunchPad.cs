1c15ebf622b7f9c97b9bd461777f7d4f5984ad46
Dendi <freshmindfreak@gmail.com>
2019-02-03 23:55:24 +0100

    launchpad first concept

---
+++ Assets/LaunchPad.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchPad : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Player")) {
+			GameController.Instance.currentWorm.AddForce(transform.up * ConfigDatabase.Instance.launchPadForce);
+		}
+	}
+}
