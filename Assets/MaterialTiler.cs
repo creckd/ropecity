2b24329119f3952ee3b0eb072f344760cdf906c2
Dendi <freshmindfreak@gmail.com>
2019-02-25 23:58:44 +0100

    LevelButton Lockolt, completed, és aktív állapotok:)

---
+++ Assets/MaterialTiler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialTiler : MonoBehaviour {
+
+	private Material mat;
+
+	public float speed = 1f;
+
+	private float timer;
+
+	private void Awake() {
+		mat = GetComponent<LineRenderer>().material;
+	}
+
+	private void Update() {
+		timer = Mathf.Repeat(timer + Time.unscaledDeltaTime * speed, 1f);
+		mat.SetTextureOffset("_MainTex", new Vector2(timer, 0f));
+	}
+}
