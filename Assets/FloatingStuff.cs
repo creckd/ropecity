40a0ba65c58f3fdd621033b5a7e37be69ac5abc7
Dendi <freshmindfreak@gmail.com>
2019-04-09 19:05:05 +0200

    dinamikus ágyú

---
+++ Assets/FloatingStuff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingStuff : MonoBehaviour {
+
+	public float amplitude = 1f;
+	public float speed = 1f;
+
+	Vector3 defaultPosition;
+
+
+	private void Awake() {
+		defaultPosition = transform.position;
+	}
+
+	private void Update() {
+		transform.position = defaultPosition + Vector3.up * Mathf.Sin(Mathf.Repeat(Time.realtimeSinceStartup * speed, 360f) * Mathf.Deg2Rad) * amplitude;
+	}
+}
