c08dc18051be838367a57126827cf3f601299332
Dendi <freshmindfreak@gmail.com>
2019-04-13 01:32:48 +0200

    loadingScreen

--- Assets/LoadingController.cs
+++ Assets/LoadingController.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class LoadingController : MonoBehaviour {
 
+	public float minimumLoadingTime = 1f;
+
 	public static void LoadScene(string sceneName) {
 		Messenger.Instance.SendMessage(LoadingSceneInfoTag, sceneName);
 		SceneManager.LoadScene("Loading");
@@ -13,9 +15,21 @@ public class LoadingController : MonoBehaviour {
 	public const string LoadingSceneInfoTag = "LSIT";
 
 	private void Start() {
+		Time.timeScale = 1f;
 		object msg = null;
 		if (Messenger.Instance.GetMessage(LoadingSceneInfoTag, out msg)) {
-			SceneManager.LoadScene(msg.ToString());
+			StartCoroutine(LoadSceneAsync(msg.ToString()));
 		}
+	}
+
+	IEnumerator LoadSceneAsync(string sceneToLoad) {
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToLoad);
+		asyncLoad.allowSceneActivation = false;
+		float timeStartedLoading = Time.realtimeSinceStartup;
+
+		while (asyncLoad.progress < 0.9f || Time.realtimeSinceStartup - timeStartedLoading < minimumLoadingTime)
+			yield return null;
+
+		asyncLoad.allowSceneActivation = true;
 	}
 }
