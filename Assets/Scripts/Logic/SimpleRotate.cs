eb2d6f0abfcab3fcc7caae4a51f0e93cf5ad7771
Dendi <freshmindfreak@gmail.com>
2019-04-13 21:02:04 +0200

    new powercube

--- Assets/Scripts/Logic/SimpleRotate.cs
+++ Assets/Scripts/Logic/SimpleRotate.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class SimpleRotate : MonoBehaviour {
 
+	public bool randomizedStartRotation = false;
 	public Vector3 axis;
 	public float speed;
+
+	private void Start() {
+		if (randomizedStartRotation)
+			transform.Rotate(new Vector3(axis.x * Random.Range(0f, 360f), axis.y * Random.Range(0f, 360f), axis.z * Random.Range(0f, 360f)));
+	}
 
 	public void Update() {
 		transform.Rotate(axis * speed * Time.unscaledDeltaTime);
