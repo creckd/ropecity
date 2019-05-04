893f4f96af31bfdb2246bbf24b9cdff4f4056579
Creckd <daniel.nagy@invictus.com>
2019-05-04 17:13:39 +0200

    manual line renderer fader

---
+++ Assets/ManualLineRendererFader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualLineRendererFader : MonoBehaviour {
+
+	private LineRenderer lineRenderer;
+	private Material materialToFade;
+
+	public string colorPropertyName = "_TintColor";
+
+	private Color defaultColor;
+
+	public float alphaValue = 1f;
+
+	private void Awake() {
+		lineRenderer = GetComponent<LineRenderer>();
+		materialToFade = lineRenderer.material;// = Instantiate(lineRenderer.material);
+
+		defaultColor = materialToFade.GetColor(colorPropertyName);
+	}
+
+	private void Update() {
+		materialToFade.SetColor(colorPropertyName, new Color(defaultColor.r,defaultColor.g,defaultColor.b, alphaValue));
+	}
+}
