8ba61eb88587fb6d1be800c3d720420ea7faa470
Creckd <daniel.nagy@invictus.com>
2019-05-23 13:40:21 +0200

    invictus splash

---
+++ Assets/SplashController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour {
+
+	public float splashDurationInSeconds = 3f;
+
+	private void Start() {
+		StartCoroutine(WaitAndLoadMainMenu(splashDurationInSeconds));
+	}
+
+	IEnumerator WaitAndLoadMainMenu(float secondsToWait) {
+		yield return new WaitForSeconds(secondsToWait);
+		SceneManager.LoadScene("MainMenu");
+	}
+}
