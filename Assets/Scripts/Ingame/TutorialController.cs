8870f043ed70edced13ef3d1629c00e321f265e8
Dendi <freshmindfreak@gmail.com>
2019-05-29 23:45:36 +0200

    kis cleanup és bonyibb tutorial elkezdve

---
+++ Assets/Scripts/Ingame/TutorialController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour {
+
+	private static TutorialController instance = null;
+	public static TutorialController Instance {
+		get {
+			if (instance == null)
+				instance = FindObjectOfType<TutorialController>();
+			return instance;
+		}
+	}
+
+	public void InitializeTutorial() {
+
+	}
+
+	private void Update() {
+		
+	}
+}
