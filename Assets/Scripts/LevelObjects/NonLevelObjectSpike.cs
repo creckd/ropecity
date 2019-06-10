b43d57540d32cb856a239f7eadb6f52e272d6213
Dendi <freshmindfreak@gmail.com>
2019-06-10 18:16:30 +0200

    new elem

--- Assets/Scripts/LevelObjects/NonLevelObjectSpike.cs
+++ Assets/Scripts/LevelObjects/NonLevelObjectSpike.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Spike : LevelObject {
+public class NonLevelObjectSpike : MonoBehaviour {
 
 	private bool isRetractableSpike = true;
 
