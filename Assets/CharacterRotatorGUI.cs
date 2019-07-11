cf346346b66a9b3fafa298ecb6416b8375748664
Dendi <freshmindfreak@gmail.com>
2019-07-11 23:09:29 +0200

    character selection panel started

---
+++ Assets/CharacterRotatorGUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRotatorGUI : MonoBehaviour {
+
+	public RenderTexture rt;
+
+	private void Start() {
+		rt.width = Screen.width;
+		rt.height = Screen.height;
+	}
+}
