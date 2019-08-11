4f296eafe127edda0fd717a179f3cf26a0f092db
Dendi <freshmindfreak@gmail.com>
2019-08-11 23:31:19 +0200

    blob upgrade

--- Assets/Scripts/Databases/SaveDatabases/GeneralSaveDatabase.cs
+++ Assets/Scripts/Databases/SaveDatabases/GeneralSaveDatabase.cs
@@ -9,7 +9,15 @@ public class GeneralSaveDatabase {
 
 	public int lastPlayedLevelIndex = -1;
 	public CharacterSaveData[] charactersSaveData;
-	public CharacterType currentlyEquippedCharacterType = CharacterType.Worm;
+	//public CharacterType currentlyEquippedCharacterType = CharacterType.Worm;
+	public CharacterType currentlyEquippedCharacterType {
+		get {
+			return CharacterType.Blob;
+		}
+		set {
+
+		}
+	} 
 
 	public void CreateEmpty() {
 		lastPlayedLevelIndex = -1;
