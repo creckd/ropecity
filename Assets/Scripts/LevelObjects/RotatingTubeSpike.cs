021025a3411ec2158c09d227ad2d4a3075efe313
Dendi <freshmindfreak@gmail.com>
2019-08-15 21:34:50 +0200

    new map

---
+++ Assets/Scripts/LevelObjects/RotatingTubeSpike.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RotatingTubeSpike : LevelObject {
+
+	private Material tubeMat;
+
+	private void Start() {
+		tubeMat = GetComponentInChildren<MeshRenderer>().material;
+		//tubeMat.SetVector("_TilingDirection", data.flowDirection);
+		BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+		if (Mathf.Abs(transform.position.z) > 5f)
+			boxCollider.enabled = false;
+	}
+
+	protected override void Update() {
+		base.Update();
+		tubeMat.SetFloat("_WorldLiquidHeight", transform.position.y);
+	}
+
+}
