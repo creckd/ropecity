8532ce2e1b7b81614c54a043ce1c9544434a9e91
Dendi <freshmindfreak@gmail.com>
2019-09-27 22:16:11 +0200

    xcode postprocess szarnyprobalgatas:D

---
+++ Assets/Editor/RopeCityPlistEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Collections;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+
+public class RopeCityPlistEditor {
+
+	[PostProcessBuild]
+	public static void ChangeXcodePlist(BuildTarget buildTarget, string pathToBuiltProject) {
+
+		if (buildTarget == BuildTarget.iOS) {
+
+			// Get plist
+			string plistPath = pathToBuiltProject + "/Info.plist";
+			PlistDocument plist = new PlistDocument();
+			plist.ReadFromString(File.ReadAllText(plistPath));
+
+			// Get root
+			PlistElementDict rootDict = plist.root;
+
+			// ADMOB
+			var admobKey = "GADApplicationIdentifier";
+			rootDict.SetString(admobKey, "ca-app-pub-9539815930599175~9209076344");
+
+			// Write to file
+			File.WriteAllText(plistPath, plist.WriteToString());
+		}
+	}
+}
